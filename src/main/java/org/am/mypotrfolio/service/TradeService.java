6bf8d7235d8f42066cf2b5d40cae23fafb14923f
Munish Kumar <40138449+ssd2658@users.noreply.github.com>
2025-08-21 13:52:05 +0530

    feat: add TradeService interface for processing trade file documents

--- src/main/java/org/am/mypotrfolio/service/TradeService.java
+++ src/main/java/org/am/mypotrfolio/service/TradeService.java
@@ -5,9 +5,6 @@ import java.util.List;
 import org.am.mypotrfolio.domain.common.DocumentRequest;
 import org.am.mypotrfolio.model.trade.TradeModel;
 
-/**
- * Service interface for processing trade-related documents
- */
 public interface TradeService {
 
     List<TradeModel> processTradeFile(DocumentRequest documentRequest);
